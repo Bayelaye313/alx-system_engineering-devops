480837f7cfeb08c7f647b9967917ca0eae639969
Bayelaye313 <bayelaye0510@gmail.com>
2024-07-03 19:21:40 +0000

    retest pakintall

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,5 @@
-# Ensure that Python 3 and pip3 are installed
-package { 'python3':
-  ensure => installed,
-}
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Install Flask version 2.1.0 using pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+# Install package
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
