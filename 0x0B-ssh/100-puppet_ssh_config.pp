69a02968b9263086a005c6e1082764931fbc852f
bayelaye313 <bayelaye0510@gmail.com>
2024-07-20 12:10:14 -0700

    change path file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # Définir la ressource pour désactiver l'authentification par mot de passe
 file_line { 'Turn off passwd auth':
-  path  => '2-ssh_config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
   match => '^PasswordAuthentication',
 }
 
 # Définir la ressource pour spécifier le fichier de clé privée
 file_line { 'Declare identity file':
-  path  => '2-ssh_config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
   match => '^IdentityFile',
 }
