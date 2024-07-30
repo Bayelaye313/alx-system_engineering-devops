b139ed4dd7b6e37ad7fada6acbee18f5a4cfa12d
bayelaye313 <bayelaye0510@gmail.com>
2024-07-30 12:27:42 +0000

    use puppet for ngix conf

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,38 @@
-# Install and configure nginx
-package { 'jfryman-nginx':
-  ensure => installed,
+# site.pp
+class { 'nginx': }
+
+nginx::resource::vhost { 'default':
+  ensure       => present,
+  listen_port  => 80,
+  server_name  => '_',
+  root         => '/var/www/html',
+  index_files  => ['index.html'],
 }
 
-include nginx
-
-class { 'nginx':
-  manage_repo    => true,
-  package_source => 'nginx-stable',
+# Créer le fichier index.html avec "Hello World!"
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Class['nginx'],
 }
 
-nginx::resource::server { '34.73.76.135':
-  listen_port      => 80,
-  www_root         => '/var/www/html/',
-  vhost_cfg_append => { 'rewrite' => '^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent' },
+# Ajouter une redirection 301 pour /redirect_me
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/vhost/default.erb'),
+  require => Class['nginx'],
 }
 
-file { 'index':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello world!',
+# Activer le site par défaut
+file { '/etc/nginx/sites-enabled/default':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+}
+
+# Redémarrer Nginx pour appliquer les changements
+exec { 'restart_nginx':
+  command     => '/usr/sbin/service nginx restart',
+  refreshonly => true,
+  subscribe   => File['/etc/nginx/sites-available/default'],
 }
