2a0f1bb804b9e8f8bd10b91b3017caa9bc10e4b6
bayelaye313 <bayelaye0510@gmail.com>
2024-08-17 21:43:28 +0000

    test

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,18 +1,5 @@
-# 0-strace_is_your_friend.pp
-exec { 'create-apache-directory':
-  command => '/bin/mkdir -p /var/www/html',
-  onlyif  => '/bin/test ! -d /var/www/html',
-}
-
-file { '/var/www/html':
-  ensure => directory,
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode   => '0755',
-}
-
-service { 'apache2':
-  ensure => running,
-  enable => true,
-  require => Exec['create-apache-directory'],
+# Fix typo in WordPress settings file
+exec { 'Fix typo in filename':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
 }
